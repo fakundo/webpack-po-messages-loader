fd69555b7ce44e6cd03d2dbf4193731bc77b3808
fakundo <fakundo.roman@gmail.com>
2017-07-12 19:28:33 +0300

    Add nodejs require hook

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
 ## Webpack Messages Loader
 
 1. Loads messages from .po files
-2. Gives a special token to each file 
-3. Collect messages with associated tokens in output files based on .po filenames
-4. Returns token associated with .po file
-
-## Output file example 
-### ru.json
-```javascript
-{
-  "token1": { /* messages from dir1/ru.po */ },
-  "token2": { /* messages from dir2/ru.po */ },
-  "token3": { /* messages from dir3/ru.po */ }
-}
-```
+2. Returns messages as json
--- index.js
+++ index.js
@@ -1,53 +1,19 @@
-var loaderUtils = require('loader-utils');
 var po2json = require('po2json');
-var fs = require('fs-extra');
-var path = require('path');
-var fnv = require('fnv');
-
-var collection = {};
-
-function createToken(request) {
-  var h = new fnv.FNV();
-  h.update(Buffer(request));
-  return h.digest('hex');
-}
-
-/**
- * @todo Add require hook for SSR
- */
 
 module.exports = function () {};
 
 module.exports.pitch = function (remainingRequest) {
   if (this.cacheable) this.cacheable();
   this.addDependency(remainingRequest);
-
   var callback = this.async();
-  var basename = path.basename(remainingRequest, '.po');
-  var options = loaderUtils.getOptions(this);
-  var token = createToken(remainingRequest);
-  var source = 'module.exports = ' + JSON.stringify(token) + ';';
 
-  if (options && options.outputDir) {
-    po2json.parseFile(remainingRequest, { format: 'jed1.x' }, function (err, data) {
-      if (err) {
-        this.emitError(err);
-      } else {
-        var messages = data.locale_data.messages;
-        delete messages[''];
-        if (!collection[basename]) collection[basename] = {};
-        collection[basename][token] = messages;
-
-        fs.outputFile(
-          path.resolve(options.outputDir, basename + '.json'),
-          JSON.stringify(collection[basename]),
-          function (error) {
-            callback(error, source);
-          }
-        );
-      }
-    });
-  } else {
-    callback(null, source);
-  }
+  po2json.parseFile(remainingRequest, { format: 'jed1.x' }, function (err, content) {
+    if (err) {
+      callback(err);
+    } else {
+      var messages = content.locale_data.messages;
+      delete messages[''];
+      callback(null, 'module.exports = ' + JSON.stringify(messages) + ';');
+    }
+  });
 };
